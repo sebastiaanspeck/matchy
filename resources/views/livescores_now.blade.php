3dff69750568d6c84d1db04155e6740aa609d5e1
Sebastiaan Speck <shem.speck@gmail.com>
2018-09-25 21:11:30 +0200

    Added minute displaying
    Added minute displaying

--- resources/views/livescores_now.blade.php
+++ resources/views/livescores_now.blade.php
@@ -29,7 +29,19 @@
                                 echo "<td scope='row'>" . $homeTeam->name . "</td>";
                                 echo "<td scope='row'>" . $awayTeam->name . "</td>";
                                 echo "<td scope='row'>" . $livescore->scores->localteam_score . " - " . $livescore->scores->visitorteam_score . "</td>";
-                                echo "<td scope='row'>" . date('Y-m-d H:i', strtotime($livescore->time->starting_at->date_time)) . "</td>";
+                                if(in_array($livescore->time->status, array('LIVE', 'HT', 'ET', 'PEN_LIVE', 'AET', 'BREAK'))) {
+                                    if($livescore->time->status == 'HT') {
+                                        echo "<td scope='row'>HT</td>";
+                                    } elseif ($livescore->time->minute == 'None' && $livescore->time->added_time == 0) {
+                                        echo "<td scope='row'>0&apos;</td>";
+                                    } elseif(in_array($livescore->time->added_time, array(0, 'None'))) {
+                                        echo "<td scope='row'>" . $livescore->time->minute . "&apos;</td>";
+                                    } elseif(!in_array($livescore->time->added_time, array(0, 'None'))) {
+                                        echo "<td scope='row'>" . $livescore->time->minute . "&apos;+" . $livescore->time->added_time . "</td>";
+                                    }
+                                } else {
+                                    echo "<td scope='row'>" . date('Y-m-d H:i', strtotime($livescore->time->starting_at->date_time)) . "</td>";
+                                }
                                 echo "<td scope='row'><a href='#'>Details</a></td>"; //link to details page (fixtures/{id})
                             echo "</tr>";
                         } else {
@@ -37,10 +49,10 @@
                                 echo "<br><h3>" . $league->name . "</h3>"; // add link to league-details page
                                 echo "<thead>";
                                     echo "<tr>";
-                                        echo "<th scope='col' width='35%'>Home team</th>";
-                                        echo "<th scope='col' width='35%'>Away team</th>";
+                                        echo "<th scope='col' width='37.5%'>Home team</th>";
+                                        echo "<th scope='col' width='37.5%'>Away team</th>";
                                         echo "<th scope='col' width='10%'>Score</th>";
-                                        echo "<th scope='col' width='15%'>Date and time</th>";
+                                        echo "<th scope='col' width='10%'>Time</th>";
                                         echo "<th scope='col' width='5%'>Details</th>";
                                     echo "</tr>";
                                 echo "</thead>";
@@ -49,7 +61,19 @@
                                         echo "<td scope='row'>" . $homeTeam->name . "</td>";
                                         echo "<td scope='row'>" . $awayTeam->name . "</td>";
                                         echo "<td scope='row'>" . $livescore->scores->localteam_score . " - " . $livescore->scores->visitorteam_score . "</td>";
-                                        echo "<td scope='row'>" . date('Y-m-d H:i', strtotime($livescore->time->starting_at->date_time)) . "</td>";
+                                        if(in_array($livescore->time->status, array('LIVE', 'HT', 'ET', 'PEN_LIVE', 'AET', 'BREAK'))) {
+                                            if($livescore->time->status == 'HT') {
+                                                echo "<td scope='row'>HT</td>";
+                                            } elseif ($livescore->time->minute == 'None' && $livescore->time->added_time == 0) {
+                                                echo "<td scope='row'>0'</td>";
+                                            } elseif(in_array($livescore->time->added_time, array(0, 'None'))) {
+                                                echo "<td scope='row'>" . $livescore->time->minute . "&apos;</td>";
+                                            } elseif(!in_array($livescore->time->added_time, array(0, 'None'))) {
+                                                echo "<td scope='row'>" . $livescore->time->minute . "&apos;+" . $livescore->time->added_time . "</td>";
+                                            }
+                                        } else {
+                                            echo "<td scope='row'>" . date('Y-m-d H:i', strtotime($livescore->time->starting_at->date_time)) . "</td>";
+                                        }
                                         echo "<td scope='row'><a href='#'>Details</a></td>"; //link to details page (fixtures/{id})
                                     echo "<tr>";
                         }
