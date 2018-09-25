0671a04566f97fd557d44ddf230bd797e15e3137
Sebastiaan Speck <shem.speck@gmail.com>
2018-09-25 17:08:50 +0200

    Reverse to home.blade without scrollbar
    Reverse to home.blade without scrollbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,62 +1,42 @@
 @extends('layouts.default')
 
 @section('style')
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 18px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
+    .container {
+        text-align: center;
+    }
+    .title {
+        font-size: 84px;
+    }
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 18px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+    .div-centered {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
 @endsection
 
 @section('content')
-    <div class="flex-center position-ref full-height">
-        <div class="content">
+    <div class="container">
+        <div class="div-centered">
             <div class="title m-b-md">
                 {{ config('app.name') }}
             </div>
-            
+
             <div class="links">
                 <a href="{{action('SoccerAPI\SoccerAPIController@viewAllLeagues')}}"> Leagues </a>
+                <a href="{{action('SoccerAPI\SoccerAPIController@livescores', ['type' => 'today'])}}"> Today's fixtures </a>
             </div>
         </div>
     </div>
