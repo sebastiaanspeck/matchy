06dc656cb6e6caa3e0cc9d4eccb56600a3fd58f2
Sebastiaan Speck <shem.speck@gmail.com>
2018-09-25 13:06:45 +0200

    Added bootstrap styled table
    Added bootstrap styled table

--- resources/views/fixtures_between.blade.php
+++ resources/views/fixtures_between.blade.php
@@ -17,33 +17,28 @@
                             echo "<tr>";
                                 echo "<td>" . $homeTeam->name . "</td>";
                                 echo "<td>" . $awayTeam->name . "</td>";
-                                echo "<td>" . $fixture->scores->ft_score . "</td>";
-                                echo "<td>" . $fixture->time->starting_at->date_time . "</td>";
                                 echo "<td>" . $fixture->id . "</td>";
+                                echo "<td scope='row'>" . $fixture->scores->ft_score . "</td>";
+                                echo "<td scope='row'>" . $fixture->time->starting_at->date_time . "</td>";
                             echo "</tr>";
                         } else {
                             echo "<table class='table table-responsive' width='100%'>";
                                 echo "<br><h3>" . $league->name . "</h3>"; // add link to league-details page
-                                echo "<col style='width:35%'>";
-                                echo "<col style='width:35%'>";
-                                echo "<col style='width:10%'>";
-                                echo "<col style='width:20%'>";
-                                echo "<thead>";
+                                echo "<thead class='thead-light'>";
                                     echo "<tr>";
-                                        echo "<th>Home team</th>";
-                                        echo "<th>Away team</th>";
-                                        echo "<th>Result</th>";
-                                        echo "<th>Date and time</th>";
-                                        echo "<th>id</th>";
+                                        echo "<th scope='col'>Home team</th>";
+                                        echo "<th scope='col'>Away team</th>";
+                                        echo "<th scope='col'>Result</th>";
+                                        echo "<th scope='col'>Date and time</th>";
                                     echo "</tr>";
                                 echo "</thead>";
                                 echo "<tbody>";
                                     echo "<tr>";
                                         echo "<td>" . $homeTeam->name . "</td>";
                                         echo "<td>" . $awayTeam->name . "</td>";
-                                        echo "<td>" . $fixture->scores->ft_score . "</td>";
-                                        echo "<td>" . $fixture->time->starting_at->date_time . "</td>";
                                         echo "<td>" . $fixture->id . "</td>";
+                                        echo "<td scope='row'>" . $fixture->scores->ft_score . "</td>";
+                                        echo "<td scope='row'>" . $fixture->time->starting_at->date_time . "</td>";
                                     echo "<tr>";
                         }
                         $last_league_id = $fixture->league_id;
