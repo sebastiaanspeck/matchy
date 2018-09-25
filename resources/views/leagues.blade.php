a2f0ce731e420833b5cd523560b4a10108189c4e
Sebastiaan Speck <shem.speck@gmail.com>
2018-09-25 12:59:37 +0200

    Added flags
    Added flags

--- resources/views/leagues.blade.php
+++ resources/views/leagues.blade.php
@@ -19,7 +19,16 @@
                         <tr>
                             {{--<td><a href="{{action('SoccerAPI\SoccerAPIController@viewLeagueDetails', ['league_id' => $project['id']])}}"> {{$league->'name'}} </a></td>--}}
                             <td> {{$league->name}} </td>
-                            <td> {{$league->country->data->name}} </td>
+                            @php
+                                if($league->country->data->extra != null) {
+                                    $flag_code = strtolower($league->country->data->extra->iso);
+                                    echo "<td>" . $league->country->data->name . "&nbsp;&nbsp; <img src=\"flags/" . $flag_code . ".gif\"> </td>";
+                                } elseif ($league->country->data->name == "Europe") {
+                                    echo "<td>" . $league->country->data->name . "&nbsp;&nbsp; <img src=\"flags/europeanunion.gif\"> </td>";
+                                } else {
+                                    echo "<td>" . $league->country->data->name . "</td>";
+                                }
+                            @endphp
                         </tr>
                     @endforeach
                     </tbody>
