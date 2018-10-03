49ea4eba31ad4701465a8ddfb3bc6b5c3d4d8382
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-03 19:55:06 +0200

    Updated home page
    Added badge to livescores - now

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,18 @@
     .container {
         text-align: center;
     }
+
+    .div-centered {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
     .title {
         font-size: 84px;
     }
+
     .links > a {
         color: #636b6f;
         padding: 0 25px;
@@ -16,27 +25,17 @@
         text-decoration: none;
         text-transform: uppercase;
     }
-    .m-b-md {
-        margin-bottom: 30px;
-    }
-    .div-centered {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
 @endsection
 
 @section('content')
     <div class="container">
         <div class="div-centered">
-            <div class="title m-b-md">
-                {{ config('app.name') }}
-            </div>
+            <div class="title">{{ config('app.name') }}</div>
 
             <div class="links">
                 <a href="{{route('leagues')}}"> Leagues </a>
-                <a href="{{route('fixturesByDate', ['day' => 'today'])}}"> Today's fixtures </a>
+                <a href="{{route('livescores', ['type' => 'today'])}}"> Today's fixtures </a>
+                <a href="{{route('livescores', ['type' => 'now'])}}"> Live now </a>
             </div>
         </div>
     </div>
