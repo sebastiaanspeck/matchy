3489bbb5033d7512315417b02a24cb22d97d12e1
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-03 19:31:24 +0200

    Added LiveServiceProvider
    Added LiveServiceProvider to show the number of livematches as a badge in the navbar

---
+++ app/Providers/LiveServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use View;
+use Illuminate\Support\ServiceProvider;
+use App\Http\Controllers\SoccerAPI\SoccerAPIController;
+
+class LiveServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('layouts/default', function($view){
+
+            $soccerAPIController = new SoccerAPIController();
+
+            $count = $soccerAPIController->countLivescores();
+
+            $view->with('live', $count);
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
