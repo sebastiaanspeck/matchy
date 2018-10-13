2fa551f2acbbe052bcbd436d1b44b92c7f15b9a1
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2018-10-14 01:09:59 +0700

    add bahasa language

---
+++ resources/lang/id/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    | source :
+    | https://github.com/caouecs/Laravel-lang/blob/master/src/id/auth.php
+    |
+    */
+
+    'failed'   => 'Identitas tersebut tidak cocok dengan data kami.',
+    'throttle' => 'Terlalu banyak usaha masuk. Silahkan coba lagi dalam :seconds detik.',
+
+];
---
+++ resources/lang/id/passwords.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    | source :
+    | https://github.com/caouecs/Laravel-lang/blob/master/src/id/passwords.php
+    |
+    */
+
+    'password' => 'Kata sandi minimal harus memiliki enam karakter dan cocok dengan konfirmasi.',
+    'reset'    => 'Kata sandi Anda sudah direset!',
+    'sent'     => 'Kami sudah mengirim surel yang berisi tautan untuk mereset kata sandi Anda!',
+    'token'    => 'Token pengaturan ulang kata sandi tidak sah.',
+    'user'     => 'Kami tidak dapat menemukan pengguna dengan alamat surel tersebut.',
+
+];
