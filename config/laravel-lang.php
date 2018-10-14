cda4624918b0ccb0caf2e5597bcd9b3233f3b6dc
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-14 10:06:47 +0200

    Added arcanedev/laravel-lang package
    With this package, you can update the language-files quickly

---
+++ config/laravel-lang.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /* -----------------------------------------------------------------
+     |  The vendor path
+     | -----------------------------------------------------------------
+     */
+
+    /** @link      https://github.com/caouecs/Laravel-lang */
+    'vendor'    => base_path('vendor/caouecs/laravel-lang/src'),
+
+    /* -----------------------------------------------------------------
+     |  Supported locales
+     | -----------------------------------------------------------------
+     | If you want to limit your translations, set your supported locales list.
+     */
+
+    'locales'   => ['id', 'nl'],
+
+    /* -----------------------------------------------------------------
+     |  Check Settings
+     | -----------------------------------------------------------------
+     */
+
+    'check'     => [
+        'ignore'  => [
+            'validation.custom',
+            'validation.attributes',
+        ],
+    ],
+
+];
