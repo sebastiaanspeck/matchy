660804294efcbfd3dea75e2a48d4f0c985f1b828
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-15 16:46:48 +0200

    Added cup-rounds translation-files
    Added cup-rounds translation-files

---
+++ resources/lang/en/cup_rounds.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cup Rounds Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for labels for the cups.
+    |
+    */
+
+    '1st Round'                     => '1st Round',
+    '2nd Round Qualifying'          => '2nd Round Qualifying',
+    '2nd Round Qualifying Replays'  => '2nd Round Qualifying Replays',
+
+    '16th Finals'                   => '16th Finals',
+
+];
---
+++ resources/lang/nl/cup_rounds.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cup Rounds Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for labels for the cups.
+    |
+    */
+
+    '1st Round'                     => '1e Ronde',
+    '2nd Round Qualifying'          => '2e Ronde Kwalificatie',
+    '2nd Round Qualifying Replays'  => '2e Ronde Kwalificatie Replays',
+
+    '16th Finals'                   => '16e Finales',
+
+];
