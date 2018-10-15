f72157c477e97cb815bf01edee282938641b77ce
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-15 09:42:47 +0200

    Copied English application.php to German folder
    Copied English application.php to German folder - needs to be translated in German. I will open an issue for it

---
+++ resources/lang/de/application.php
@@ -0,0 +1,101 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for labels for the application.
+    |
+    */
+
+    'league'                    => 'league',
+    'leagues'                   => 'leagues',
+    'Leagues'                   => 'Leagues',
+    'All leagues'               => 'All leagues',
+
+    'League name'               => 'League name',
+    'Country'                   => 'Country',
+
+    'fixture'                   => 'fixture',
+    'fixtures'                  => 'fixtures',
+    'Fixtures'                  => 'Fixtures',
+    'todays\'s fixtures'        => 'todays\'s fixtures',
+    'last fixtures'             => '{1} Last fixture|[2,*] Upcoming :count fixtures',
+    'upcoming fixtures'         => '{1} Upcoming fixture|[2,*] Upcoming :count fixtures',
+    'Standings'                 => 'Standings',
+    'Topscorers'                => 'Topscorers',
+    'Player'                    => 'Player',
+
+    'No.'                       => 'No.',
+    'Team'                      => 'Team',
+    'Played'                    => 'Played',
+    'Won'                       => 'Won',
+    'Draw'                      => 'Draw',
+    'Lost'                      => 'Lost',
+    'Goals'                     => 'Goals',
+    'Points'                    => 'Points',
+    'Form'                      => 'Form',
+    'W'                         => 'W',
+    'D'                         => 'D',
+    'L'                         => 'L',
+
+    'live'                      => 'live',
+    'livescores'                => 'livescores',
+    'Livescores'                => 'Livescores',
+
+    'Last update'               => 'Last update',
+
+    'Venue'                     => 'Venue',
+    'Referee'                   => 'Referee',
+    'Match Summary'             => 'Match Summary',
+    'Statistics'                => 'Statistics',
+    'Lineups'                   => 'Lineups',
+    'Formation'                 => 'Formation',
+    'Starting lineups'          => 'Starting lineups',
+    'Substitutes'               => 'Substitutes',
+    'Missing players'           => 'Missing players',
+    'Coaches'                   => 'Coaches',
+
+    'Total shots'               => 'Total shots',
+    'Shots on goal'             => 'Schots on goal',
+    'Shots of goal'             => 'Shots of goal',
+    'Blocked shots'             => 'Blocked shots',
+    'Shots inside box'          => 'Shots inside box',
+    'Shots outside box'         => 'Shots outside box',
+    'Total passes'              => 'Total passes',
+    'Accurate passes'           => 'Accurate passes',
+    'Passing percentage'        => 'Passing percentage',
+    'Total attacks'             => 'Total attacks',
+    'Dangerous attacks'         => 'Dangerous attacks',
+    'Fouls'                     => 'Fouls',
+    'Corners'                   => 'Corners',
+    'Offside'                   => 'Offside',
+    'Ball possession'           => 'Ball possession',
+    'Yellow cards'              => 'Yellow cards',
+    'Red cards'                 => 'Red cards',
+    'Saves'                     => 'Saves',
+    'Substitutions'             => 'Substitutions',
+    'Goal kicks'                => 'Goal kicks',
+    'Goal attempts'             => 'Goal attempts',
+    'Free kicks'                => 'Free kicks',
+    'Throw-ins'                 => 'Throw-ins',
+    'Ball safe'                 => 'Ball safe',
+
+    'now'                       => 'now',
+    'today'                     => 'today',
+    'Today'                     => 'Today',
+    'tomorrow'                  => 'tomorrow',
+    'Tomorrow'                  => 'Tomorrow',
+    'yesterday'                 => 'yesterday',
+    'Yesterday'                 => 'Yestterday',
+
+    'msg_too_much_results'      => 'We only show the first :count results. The shown data might be incomplete. We try to fix this in a later version.',
+    'msg_no_livescores_now'     => 'No livescores live now.',
+    'msg_no_livescores_today'   => 'No livescores for today.',
+    'msg_no_matches_found'      => 'No matches found for :date.',
+    'msg_no_data'               => 'is not (yet) available for this fixture|are not (yet) available for this fixture',
+
+];
