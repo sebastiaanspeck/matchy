7bf7064755ac0b59a29637064f13f90d6e15be50
Sandro Athaide Coelho <sandro.athaide@dowjones.com>
2018-10-16 15:55:41 -0300

    Adding German translation for countries

---
+++ resources/lang/de/countries.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Countries Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for labels for the countries.
+    |
+    */
+
+    'Argentina'                     => 'Argentinien',
+    'Australia'                     => 'Australien',
+    'Austria'                       => 'Österreich',
+    'Belarus'                       => 'Weißrussland',
+    'Brazil'                        => 'Brasilien',
+    'Bosnia and Herzegovina'        => 'Bosnien und Herzegowina',
+    'Bulgaria'                      => 'Bulgarien',
+    'Chile'                         => 'Chile',
+    'Colombia'                      => 'Kolumbien',
+    'Croatia'                       => 'Kroatien',
+    'Cyprus'                        => 'Republik Zypern',
+    'Czech Republic'                => 'Tschechien',    
+    'Denmark'                       => 'Dänemark',
+    'Ecuador'                       => 'Ecuador',
+    'England'                       => 'England',
+    'Europe'                        => 'Europe',
+    'France'                        => 'Frankreich',
+    'Georgia'                       => 'Georgien',
+    'Germany'                       => 'Deutschland',
+    'Ghana'                         => 'Ghana',
+    'Greece'                        => 'Griechenland',
+    'Iran'                          => 'Iran',
+    'Iraq'                          => 'Irak',
+    'Italy'                         => 'Italien',
+    'Ivory Coast'                   => 'Elfenbeinküste',
+    'Japan'                         => 'Japan',
+    'Kosovo'                        => 'Kosovo',
+    'Mali'                          => 'Mali',
+    'Mexico'                        => 'Mexiko',
+    'Netherlands'                   => 'Niederlande',
+    'New Caledonia'                 => 'Neukaledonien',
+    'Northern Ireland'              => 'Nordirland',
+    'Peru'                          => 'Peru',
+    'Portugal'                      => 'Portugal',
+    'Poland'                        => 'Polen',
+    'Republic of Ireland'           => 'Irland',
+    'Romania'                       => 'Romania',
+    'Russia'                        => 'Russia',
+    'Senegal'                       => 'Senegal',
+    'Serbia'                        => 'Serbia',
+    'Slovakia'                      => 'Slovakia',
+    'Spain'                         => 'Spain',
+    'South Africa'                  => 'South Africa',
+    'Sweden'                        => 'Sweden',
+    'Switzerland'                   => 'Switzerland',
+    'Turkey'                        => 'Turkey',
+    'Ukraine'                       => 'Ukraine',
+    'Uruguay'                       => 'Uruguay',
+    'Venezuela'                     => 'Venezuela',
+    'Wales'                         => 'Wales',
+];
