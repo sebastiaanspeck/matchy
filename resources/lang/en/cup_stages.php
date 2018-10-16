e6d3be6773d4addc317e724b267a05009e7697c8
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-16 17:58:26 +0200

    Added cup_stages.php translations
    Added cup_stages.php translations

--- resources/lang/en/cup_stages.php
+++ resources/lang/en/cup_stages.php
@@ -12,13 +12,22 @@ return [
     */
 
     '1st Round'                     => '1st Round',
+    '1st Qualifying Round'          => '1st Qualifying Round',
     '2nd Round'                     => '2nd Round',
     '2nd Round Qualifying'          => '2nd Round Qualifying',
     '2nd Round Qualifying Replays'  => '2nd Round Qualifying Replays',
+    '2nd Qualifying Round'          => '2nd Qualifying Round',
     '3rd Round'                     => '3rd Round',
+    '3rd Round Qualifying'          => '3rd Round Qualifying',
+    '3rd Round Qualifying Replays'  => '3rd Round Qualifying Replays',
+    '3rd Qualifying Round'          => '3rd Qualifying Round',
     '4th Round'                     => '4th Round',
+    '4th Preliminary Round'         => '4th Preliminary Round',
     '5th Round'                     => '5th Round',
+    '7th Round'                     => '7th Round',
+
     'Qualifying Round'              => 'Qualifying Round',
+    'Preliminary Round'             => 'Preliminary Round',
 
     'Group Stage'                   => 'Group Stage',
     '16th Finals'                   => '16th Finals',
