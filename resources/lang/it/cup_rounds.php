b0576138cd271c0091c2f48b531c189b3f661b40
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-16 08:04:09 +0200

    Added Italian language
    Added Italian language

---
+++ resources/lang/it/cup_rounds.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cup Rounds Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for labels for the cups.
+    |
+    */
+
+    '1st Round'                     => '1st Round',
+    '2nd Round Qualifying'          => '2nd Round Qualifying',
+    '2nd Round Qualifying Replays'  => '2nd Round Qualifying Replays',
+
+    '8th Finals'                    => '8th Finals',
+    '16th Finals'                   => '16th Finals',
+
+    'Play-offs'                     => 'Play-offs',
+
+];
