1736305accabd1c5add14c9d6e510bc1e80d70b6
Jorge Atempa <atempaitm@gmail.com>
2018-10-16 00:00:14 -0700

    [WIP] Translate countries to spanish language

---
+++ resources/lang/es/countries.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Countries Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for labels for the countries.
+    |
+    */
+
+    'Argentina'                     => 'Argentina',
+    'Australia'                     => 'Australia',
+    'Austria'                       => 'Austria',
+    'Brazil'                        => 'Brasil',
+    'Chile'                         => 'Chile',
+    'Colombia'                      => 'Colombia',
+    'Cyprus'                        => 'Chipre',
+    'France'                        => 'Francia',
+    'Germany'                       => 'Alemania',
+    'Greece'                        => 'Grecia',
+    'Netherlands'                   => 'Paises bajos',
+    'Ivory Coast'                   => 'Costa de Marfíl',
+    'Japan'                         => 'Japon',
+    'Peru'                          => 'Perú',
+    'Portugal'                      => 'Portugal',
+    'Poland'                        => 'Polonia',
+    'Spain'                         => 'España',
+    'Sweden'                        => 'Suecia',
+    'Switzerland'                   => 'Suiza',
+    'Ukraine'                       => 'Ucrania',
+    'Uruguay'                       => 'Uruguay',
+];
