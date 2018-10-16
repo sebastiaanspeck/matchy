ad78cf74c63db628e6e270a950bc4bb3bbf1719d
Jorge Atempa <jorge.atempa@tagwizz.com.mx>
2018-10-16 09:25:23 -0700

    Create a countries file for EN and NL language

---
+++ resources/lang/nl/countries.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Countries Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for labels for the countries.
+    |
+    */
+
+    'country' => 'country'
+];
