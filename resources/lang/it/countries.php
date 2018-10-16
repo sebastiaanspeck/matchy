1e0b04d71108b394893d27e70731c57642955f77
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-16 20:52:47 +0200

    Added countries.php
    Added countries.php - still need to be translated

---
+++ resources/lang/it/countries.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Countries Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for labels for the countries.
+    |
+    */
+
+    'Argentina'                     => 'Argentina',
+    'Australia'                     => 'Australia',
+    'Austria'                       => 'Austria',
+    'Belarus'                       => 'Belarus',
+    'Belgium'                       => 'Belgium',
+    'Brazil'                        => 'Brazil',
+    'Bosnia and Herzegovina'        => 'Bosnia and Herzegovina',
+    'Bulgaria'                      => 'Bulgaria',
+    'Chile'                         => 'Chile',
+    'Colombia'                      => 'Colombia',
+    'Costa Rica'                    => 'Costa Rica',
+    'Croatia'                       => 'Croatia',
+    'Cyprus'                        => 'Cyprus',
+    'Czech Republic'                => 'Czech Republic',    
+    'Denmark'                       => 'Denmark',
+    'Ecuador'                       => 'Ecuador',
+    'England'                       => 'England',
+    'Europe'                        => 'Europe',
+    'France'                        => 'France',
+    'Georgia'                       => 'Georgia',
+    'Germany'                       => 'Germany',
+    'Ghana'                         => 'Ghana',
+    'Greece'                        => 'Greece',
+    'Iceland'                       => 'Iceland',
+    'Iran'                          => 'Iran',
+    'Iraq'                          => 'Iraq',
+    'Italy'                         => 'Italy',
+    'Ivory Coast'                   => 'Ivory Coast',
+    'Japan'                         => 'Japan',
+    'Kosovo'                        => 'Kosovo',
+    'Korea Republic'                => 'Korea Republic',
+    'Mali'                          => 'Mali',
+    'Mexico'                        => 'Mexico',
+    'Morocoo'                       => 'Morocco',
+    'Netherlands'                   => 'Netherlands',
+    'New Caledonia'                 => 'New Caledonia',
+    'Nigeria'                       => 'Nigeria',
+    'Northern Ireland'              => 'Northern Ireland',
+    'Panama'                        => 'Panama',
+    'Peru'                          => 'Peru',
+    'Portugal'                      => 'Portugal',
+    'Poland'                        => 'Poland',
+    'Republic of Ireland'           => 'Republic of Ireland',
+    'Romania'                       => 'Romania',
+    'Russia'                        => 'Russia',
+    'Saudi Arabia'                  => 'Saudi Arabia',
+    'Senegal'                       => 'Senegal',
+    'Serbia'                        => 'Serbia',
+    'Slovakia'                      => 'Slovakia',
+    'Spain'                         => 'Spain',
+    'South Africa'                  => 'South Africa',
+    'Sweden'                        => 'Sweden',
+    'Switzerland'                   => 'Switzerland',
+    'Tunisia'                       => 'Tunisia',
+    'Turkey'                        => 'Turkey',
+    'Ukraine'                       => 'Ukraine',
+    'Uruguay'                       => 'Uruguay',
+    'Venezuela'                     => 'Venezuela',
+    'Wales'                         => 'Wales',
+    'World'                         => 'World',
+];
