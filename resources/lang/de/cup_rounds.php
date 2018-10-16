68a07684312dbc2d4ce81d319675d46889e5797e
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-16 08:03:37 +0200

    Added cup_rounds.php to German-lang folder
    Added cup_rounds.php to German-lang folder

--- resources/lang/de/cup_rounds.php
+++ resources/lang/de/cup_rounds.php
@@ -11,5 +11,13 @@ return [
     |
     */
 
+    '1st Round'                     => '1st Round',
+    '2nd Round Qualifying'          => '2nd Round Qualifying',
+    '2nd Round Qualifying Replays'  => '2nd Round Qualifying Replays',
+
+    '8th Finals'                    => '8th Finals',
+    '16th Finals'                   => '16th Finals',
+
+    'Play-offs'                     => 'Play-offs',
 
 ];
