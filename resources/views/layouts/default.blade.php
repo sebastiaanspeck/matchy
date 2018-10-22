9079a2322f5666dfeb18e2ea7b609eb856778afb
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-22 18:26:11 +0200

    Updated default layout.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     @yield("meta")
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Matchy">
+    <meta name="author" content="Sebastiaan Speck">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -23,12 +25,59 @@
 
     <!-- Styles -->
     <link href="{{ asset("css/app.css") }}" rel="stylesheet">
-
+    
     <style>
-        body {
-            padding-top: 60px!important;
-            overflow-y: scroll!important;
+        html {
+            position: relative;
+            min-height: 100%;
         }
+
+        body {
+            margin-bottom: 60px;
+            padding-top: 60px;
+            overflow-y: scroll;
+        }
+
+        .container-fluid {
+            width: 100%;
+            padding-right: 15px;
+            padding-left: 15px;
+            margin-right: auto;
+            margin-left: auto;
+        }
+        
+        .main-container {
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+        
+        .main-footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 60px;
+            line-height: 60px;
+            background-color: #E8EAF6;
+        }
+        
+        .main-footer p {
+            margin-bottom: 0;
+        }
+        
+        /*
+         * Navbar
+         */
+        
+        .navbar-brand {
+            padding: .75rem 1rem;
+            font-size: 1rem;
+        }
+        
+        .navbar-nav .nav-link {
+            padding-right: .5rem;
+            padding-left: .5rem;
+        }
+        
         a:link {
             color: black;
             background-color: transparent;
@@ -41,6 +90,10 @@
         }
         a:hover {
             color: grey;
+        }
+        
+        caption {
+            caption-side: top;
         }
 
         .won-team:link {
@@ -109,6 +162,7 @@
 
         .live {
             animation: blinker 1.5s linear infinite;
+            color: #ff0000;
         }
 
         @keyframes blinker {
@@ -121,17 +175,17 @@
     </style>
 </head>
 <body>
-<div id="app">
-
-    <nav class="navbar navbar-expand-lg navbar-light navbar-laravel fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="{{ url("/") }}">
-                    {{ config("app.name") }}
-                </a>
-            </div>
-            <div id="navbarSupportedContent" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav mr-auto">
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark p-1">
+        <!--<nav class="navbar navbar-expand-md navbar-light navbar-laravel fixed-top">-->
+            <a class="navbar-brand" href="{{ url("/") }}">
+                <i class="fa fa-futbol-o fa-fw" aria-hidden="true"></i>&nbsp;{{ config("app.name") }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownFixtures" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Lang::has("application.Fixtures") ? trans("application.Fixtures") : Log::emergency("Missing application translation for: Fixtures") . "Fixtures" }}</a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownFixtures">
@@ -155,12 +209,23 @@
                     </li>
                 </ul>
             </div>
-        </div>
-    </nav>
-
-    <main class="py-4">
+        </nav>
+    </header>
+    
+    <main role="main" class="container-fluid main-container">
         @yield("content")
     </main>
-</div>
+    
+    <footer class="main-footer">
+        <div class="container-fluid">
+            <p class="text-muted pull-left">
+                Matchy - <span class="badge badge-success">{{ trans("application.version") }} 1.5.0</span> - <a href="https://github.com/sebastiaanspeck/matchy" class="text-muted" target="_blank">Github Repo <i class="fa fa-github" aria-hidden="true"></i></a>
+            </p>
+            <p class="text-muted pull-right">
+                {{ trans("application.Created with") }} <i class="fa fa-code" aria-hidden="true"></i> & <i class="fa fa-coffee" aria-hidden="true"></i>
+            </p>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 </body>
 </html>
