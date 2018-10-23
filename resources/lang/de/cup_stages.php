97176967514f9eb9ab3d164a24c595edefca7f67
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-23 18:52:17 +0200

    Added translations

--- resources/lang/de/cup_stages.php
+++ resources/lang/de/cup_stages.php
@@ -1,32 +1,35 @@
-<?php
+<?php 
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Cup Stages Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used for labels for the cup stages.
-    |
-    */
-
-    '1st Round'                     => 'Erste Runde',
-    '2nd Round'                     => '2nd Round',
-    '2nd Round Qualifying'          => 'Zweite Runde Qualifizierung',
-    '2nd Round Qualifying Replays'  => 'Zweite Runde Qualifizierung Wiederholungsspiel',
-    '3rd Round'                     => '3rd Round',
-    '4th Round'                     => '4th Round',
-    '5th Round'                     => '5th Round',
-    'Qualifying Round'              => 'Qualifying Round',
-
-    'Group Stage'                   => 'Group Stage',
-    '16th Finals'                   => 'Sechzehntelfinale',
-    '8th Finals'                    => 'Achtelfinale',
-    'Quarter-finals'                => 'Quarter-finals',
-    'Semi-final'                    => 'Semi-final',
-    'Semi-finals'                   => 'Semi-finals',
-    '3rd Place Final'               => '3rd Place Final',
-    'Final'                         => 'Final',
-
-    'Play-offs'                     => 'Play-offs',
+    '1st Round' => 'Erste Runde',
+    '2nd Round' => '2nd Round',
+    '2nd Round Qualifying' => 'Zweite Runde Qualifizierung',
+    '2nd Round Qualifying Replays' => 'Zweite Runde Qualifizierung Wiederholungsspiel',
+    '3rd Round' => '3rd Round',
+    '4th Round' => '4th Round',
+    '5th Round' => '5th Round',
+    'Qualifying Round' => 'Qualifying Round',
+    'Group Stage' => 'Group Stage',
+    '16th Finals' => 'Sechzehntelfinale',
+    '8th Finals' => 'Achtelfinale',
+    'Quarter-finals' => 'Quarter-finals',
+    'Semi-final' => 'Semi-final',
+    'Semi-finals' => 'Semi-finals',
+    '3rd Place Final' => '3rd Place Final',
+    'Final' => 'Final',
+    'Play-offs' => 'Play-offs',
+    '1st Qualifying Round' => '',
+    '2nd Qualifying Round' => '',
+    '3rd Round Qualifying' => '',
+    '3rd Round Qualifying Replays' => '',
+    '3rd Qualifying Round' => '',
+    '4th Round Qualifying' => '',
+    '4th Preliminary Round' => '',
+    '7th Round' => '',
+    'Preliminary Round' => '',
+    'Group stage' => '',
+    'League A' => '',
+    'League B' => '',
+    'League C' => '',
+    'League D' => '',
 ];
--- resources/lang/id/cup_stages.php
+++ resources/lang/id/cup_stages.php
@@ -1,32 +1,35 @@
-<?php
+<?php 
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Cup Stages Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used for labels for the cup stages.
-    |
-    */
-
-    '1st Round'                     => 'Putaran Pertama',
-    '2nd Round'                     => 'Putaran Kedua',
-    '2nd Round Qualifying'          => 'Penyisihan Putaran Kedua',
-    '2nd Round Qualifying Replays'  => 'Siaran Ulang Penyisihan Putaran Kedua',
-    '3rd Round'                     => 'Putaran Ketiga',
-    '4th Round'                     => 'Putaran Keempat',
-    '5th Round'                     => 'Putaran Kelima',
-    'Qualifying Round'              => 'Babak Penyisihan',
-
-    'Group Stage'                   => 'Penyisihian Grup',
-    '16th Finals'                   => 'Babak 16 Besar',
-    '8th Finals'                    => 'Babak 8 Besar',
-    'Quarter-finals'                => 'Perempatfinal',
-    'Semi-final'                    => 'Semifinal',
-    'Semi-finals'                   => 'Semifinal',
-    '3rd Place Final'               => 'Perebutan Posisi Ketiga',
-    'Final'                         => 'Final',
-
-    'Play-offs'                     => 'Pertandingan Penentuan',
+    '1st Round' => 'Putaran Pertama',
+    '2nd Round' => 'Putaran Kedua',
+    '2nd Round Qualifying' => 'Penyisihan Putaran Kedua',
+    '2nd Round Qualifying Replays' => 'Siaran Ulang Penyisihan Putaran Kedua',
+    '3rd Round' => 'Putaran Ketiga',
+    '4th Round' => 'Putaran Keempat',
+    '5th Round' => 'Putaran Kelima',
+    'Qualifying Round' => 'Babak Penyisihan',
+    'Group Stage' => 'Penyisihian Grup',
+    '16th Finals' => 'Babak 16 Besar',
+    '8th Finals' => 'Babak 8 Besar',
+    'Quarter-finals' => 'Perempatfinal',
+    'Semi-final' => 'Semifinal',
+    'Semi-finals' => 'Semifinal',
+    '3rd Place Final' => 'Perebutan Posisi Ketiga',
+    'Final' => 'Final',
+    'Play-offs' => 'Pertandingan Penentuan',
+    '1st Qualifying Round' => '',
+    '2nd Qualifying Round' => '',
+    '3rd Round Qualifying' => '',
+    '3rd Round Qualifying Replays' => '',
+    '3rd Qualifying Round' => '',
+    '4th Round Qualifying' => '',
+    '4th Preliminary Round' => '',
+    '7th Round' => '',
+    'Preliminary Round' => '',
+    'Group stage' => '',
+    'League A' => '',
+    'League B' => '',
+    'League C' => '',
+    'League D' => '',
 ];
