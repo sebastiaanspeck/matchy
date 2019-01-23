5f47f9c7c7b4729a2f791acc1277dcc660902efe
Sebastiaan Speck <shem.speck@gmail.com>
2019-01-23 12:25:23 +0100

    Added TokenTest

---
+++ tests/Feature/TokenTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class TokenTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
