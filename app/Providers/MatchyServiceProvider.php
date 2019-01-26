cd56da9e4cb3fa8266317c70742199a5cb83111f
Sebastiaan Speck <shem.speck@gmail.com>
2019-01-26 14:17:26 +0100

    Added ServiceProvider
    ServiceProvider for preferences config file

---
+++ app/Providers/MatchyServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MatchyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $configPath = __DIR__.'/../../config';
+
+        $this->mergeConfigFrom($configPath.'/config.php', 'preferences');
+
+        $this->publishes([
+            $configPath.'/config.php' => config_path('preferences.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
