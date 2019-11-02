7fee07e8e721909431b94ad637cdf603c20c70d3
Sebastiaan Speck <shem.speck@gmail.com>
2019-11-02 20:29:32 +0100

    Remove duplicate code

--- app/Http/Controllers/PreferencesController.php
+++ app/Http/Controllers/PreferencesController.php
@@ -7,6 +7,7 @@ use Filebase\Filesystem\FilesystemException;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 /**
@@ -19,29 +20,12 @@ class PreferencesController extends BaseController
     /**
      * @param $teamId
      *
+     * @return RedirectResponse
      * @throws FilesystemException
-     *
-     * @return back
      */
     public function setFavoriteTeams($teamId)
     {
-        $favorite_teams = $this->getFavoriteTeams();
-
-        if (in_array($teamId, $favorite_teams)) {
-            if (($key = array_search($teamId, $favorite_teams)) !== false) {
-                unset($favorite_teams[$key]);
-            }
-        } else {
-            $favorite_teams[] = $teamId;
-        }
-
-        if (array_key_exists(0, $favorite_teams)) {
-            if ($favorite_teams[0] === '') {
-                unset($favorite_teams[0]);
-            }
-        }
-
-        $favorite_teams = implode(',', $favorite_teams);
+        $favorite_teams = $this->setFavorite($teamId, $this->getFavoriteTeams());
 
         FilebaseController::setField('favorite_teams', $favorite_teams);
 
@@ -61,28 +45,12 @@ class PreferencesController extends BaseController
     /**
      * @param $leagueId
      *
+     * @return RedirectResponse
      * @throws FilesystemException
-     *
-     * @return back
      */
     public function setFavoriteLeagues($leagueId)
     {
-        $favorite_leagues = $this->getFavoriteLeagues();
-
-        if (in_array($leagueId, $favorite_leagues)) {
-            if (($key = array_search($leagueId, $favorite_leagues)) !== false) {
-                unset($favorite_leagues[$key]);
-            }
-        } else {
-            $favorite_leagues[] = $leagueId;
-        }
-
-        if (array_key_exists(0, $favorite_leagues)) {
-            if ($favorite_leagues[0] === '') {
-                unset($favorite_leagues[0]);
-            }
-        }
-        $favorite_leagues = implode(',', $favorite_leagues);
+        $favorite_leagues = $this->setFavorite($leagueId, $this->getFavoriteLeagues());
 
         FilebaseController::setField('favorite_leagues', $favorite_leagues);
 
@@ -97,5 +65,24 @@ class PreferencesController extends BaseController
     public function getFavoriteLeagues()
     {
         return FilebaseController::getField('favorite_leagues');
+    }
+
+    public function setFavorite($id, $favorites)
+    {
+        if (in_array($id, $favorites)) {
+            if (($key = array_search($id, $favorites)) !== false) {
+                unset($favorites[$key]);
+            }
+        } else {
+            $favorites[] = $id;
+        }
+
+        if (array_key_exists(0, $favorites)) {
+            if ($favorites[0] === '') {
+                unset($favorites[0]);
+            }
+        }
+
+        return implode(',', $favorites);
     }
 }
